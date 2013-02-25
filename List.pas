9a47dc1870c3851fde310492d7fd8ab6aecdab99
Andrew Novikov <andnovator@gmail.com>
2013-02-25 22:23:11 +0400

    Создание итератора, перереализация функций List

--- List.pas
+++ List.pas
@@ -6,23 +6,84 @@ type
     prev, next: ListItems;
     value: TValue;
   end;
+  
+  Iterator = class
+  private
+    next,prev: ListItems;
+  public
+    constructor Create(initial: ListItems);
+    function Iterator.hasNext : boolean;
+    function Iterator.hasPrev : boolean;
+    function Iterator.getNext : TValue;
+    function Iterator.getPrev : TValue;
+  end;
 
+constructor Iterator.Create(initial: ListItems);
+  begin
+    next := initial;
+  end;
+
+function Iterator.hasNext : boolean;
+  begin
+    hasNext := next <> nil;
+  end;
+function Iterator.hasPrev : boolean;
+  begin
+    hasPrev := prev <> nil;
+  end;
+
+function Iterator.getNext : TValue;
+  begin
+    Assert(hasNext, 'Iterator has no next item');
+    getNext := next^.value;
+    next := next^.next;
+  end;
+function Iterator.getPrev : TValue;
+  begin
+    Assert(hasPrev, 'Iterator has no prev item');
+    getPrev := prev^.value;
+    prev := prev^.prev;
+  end;
+
+type  
   List = class
+  private
     head, tail: ListItems;
-    length:word;
+    leng:word;
     
+  public
     constructor Create;
+    function List.length:word;
     procedure List.push(val: TValue);
     function List.isEmpty : boolean;
     function List.get(ind: integer) : TValue;
-    function List.getEnd(ind: integer) : TValue;
+    function List.getFromEnd(ind: integer) : TValue;
     function List.getFirst : TValue;
     function List.getLast : TValue;
+    function List.getBegin : Iterator;
+    function List.getEnd : Iterator;
+    //function List.getIterator(ind: integer) : Iterator;
+    //procedure List.remove(Iterator)
+    //function List.find(v:TValue):Iterator
+    //function List.find(cond:function(TValue):boolean):Iterator
+    //function List.find(v:TValue, from: Iterator):Iterator
   end;
-  
+function List.getBegin : Iterator;
+  begin
+    getBegin := Iterator.Create(head);
+  end;
+function List.getEnd : Iterator;
+  begin
+    getEnd := Iterator.Create(tail);
+  end;
+function List.length:word;
+  begin
+    length:=leng;
+  end;
+
 constructor List.Create;
   begin
-    length:=0;
+    leng:=0;
     head := nil;
     tail := nil;
   end;
@@ -34,7 +95,7 @@ procedure List.push(val: TValue);
   var item:ListItems;
   begin
     new(item);
-    Inc(length);
+    Inc(leng);
     with item^ do begin
       next:=nil;
       value:=val;
@@ -60,7 +121,7 @@ function List.get(ind: integer) : TValue;
       point:=point^.next;
     get:=point^.value;
   end;
-function List.getEnd(ind: integer) : TValue;
+function List.getFromEnd(ind: integer) : TValue;
   var iterator:integer;
       point:ListItems;
   begin
@@ -68,17 +129,20 @@ function List.getEnd(ind: integer) : TValue;
     point:=tail;
     for iterator:=2 downto ind do
       point:=point^.next;
-    getEnd:=point^.value;
+    getFromEnd:=point^.value;
   end;
 function List.getFirst : TValue;
   begin
+    getFirst:=getBegin.getNext;
   end;
 function List.getLast : TValue;
   begin
+    getLast:=getEnd.getNext;
   end;
 var
   lst: List;
-  val1, val2: TValue;
+  val1, val2, v: TValue;
+  it: Iterator;
 begin
   // Test push
   lst := List.Create;
@@ -97,4 +161,10 @@ begin
   writeln( lst.length );
   Assert( lst.get(1) = val1, 'First item is wrong' );
   Assert( lst.get(2) = val2, 'Second item is wrong' );
+  
+  it := lst.getBegin;
+  while it.hasNext do begin
+    v := it.getNext;
+    writeln( v );
+  end;
 end.
